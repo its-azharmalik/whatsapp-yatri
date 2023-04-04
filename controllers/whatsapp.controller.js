7074788a99ea3389c88a1ff83378e926c3e588f9
Azhar Malik <malik_258@Azhars-MacBook-Pro.local>
2023-04-05 01:34:22 +0530

    test 5

--- controllers/whatsapp.controller.js
+++ controllers/whatsapp.controller.js
@@ -1,13 +1,14 @@
 const { e } = require('../config');
 const { User } = require('../models');
 const { imageUploaderSingle } = require('../services');
+const client = require('twilio');
 
 const sendMessage = async (messageBody, number) => {
 	try {
 		console.log('sendMessage');
 		const accountSid = 'ACa1d7033540820f01d6206b37bf6dcdc3';
 		const authToken = 'ba0d6343929c4a5bcdfd7b0756062bf0';
-		const client = require('twilio')(accountSid, authToken);
+		client(accountSid, authToken);
 
 		client.messages
 			.create({

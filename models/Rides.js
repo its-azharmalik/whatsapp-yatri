75e45f4ccfc52a76025097fee3ae16c51854a55a
Utkarsh-88034 <robbin7838@gmail.com>
2023-04-19 23:53:12 +0530

    .

--- models/Rides.js
+++ models/Rides.js
@@ -1,134 +1,136 @@
-const { default: mongoose, Mongoose } = require('mongoose');
+const { default: mongoose, Mongoose } = require("mongoose");
 
-const e = require('../config/errorList');
+const e = require("../config/errorList");
 
 const validateEmail = (email) => {
-	const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	return re.test(email);
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
 };
 
 const userSchema = new mongoose.Schema({
-	phone: {
-		type: String,
-		required: true,
-		unique: true,
-		trim: true,
-	},
-	ridesData: [
-		{
-			// timestamps: {
-			// 	cat,
-			// 	uat,
-			// 	lastSeen,
-			// },
-			currentStage: {
-				type: String,
-				default: 'NOT STARTED',
-				required: true,
-			},
-			booking: {
-				isInitiated: {
-					type: Boolean,
-					default: false,
-					required: true,
-				},
-				recievedText: {
-					type: Boolean,
-					default: false,
-					required: true,
-				},
-			},
-			startingLocation: {
-				longitude: {
-					type: String,
-					trim: true,
-				},
-				longitude: {
-					type: String,
-					trim: true,
-				},
-				isInitiated: {
-					type: Boolean,
-					default: false,
-					required: true,
-				},
-				recievedText: {
-					type: Boolean,
-					default: false,
-					required: true,
-				},
-			},
-			destinationLocation: {
-				longitude: {
-					type: String,
-					trim: true,
-				},
-				longitude: {
-					type: String,
-					trim: true,
-				},
-				isInitiated: {
-					type: Boolean,
-					default: false,
-					required: true,
-				},
-				recievedText: {
-					type: Boolean,
-					default: false,
-					required: true,
-				},
-			},
-			confirmLocation: {
-				isInitiated: {
-					type: Boolean,
-					default: false,
-					required: true,
-				},
-				recievedText: {
-					type: Boolean,
-					default: false,
-					required: true,
-				},
-				startLocationLink: {
-					type: String,
-					trim: true,
-				},
-				endLocationLink: {
-					type: String,
-					trim: true,
-				},
-			},
-			searchRides: {
-				isInitiated: {
-					type: Boolean,
-					default: false,
-					required: true,
-				},
-				driverDetails: {
-					type: String,
-					trim: true,
-				},
-				otp: {
-					type: String,
-					trim: true,
-				},
-			},
-			rideStatus: {
-				rideStarted: {
-					type: Boolean,
-					default: false,
-					required: true,
-				},
-				rideEnded: {
-					type: Boolean,
-					default: false,
-					required: true,
-				},
-			},
-		},
-	],
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  ridesData: [
+    {
+      // timestamps: {
+      // 	cat,
+      // 	uat,
+      // 	lastSeen,
+      // },
+      type: Object,
+
+      currentStage: {
+        type: String,
+        default: "NOT STARTED",
+        required: true,
+      },
+      booking: {
+        isInitiated: {
+          type: Boolean,
+          default: false,
+          required: true,
+        },
+        recievedText: {
+          type: Boolean,
+          default: false,
+          required: true,
+        },
+      },
+      startingLocation: {
+        longitude: {
+          type: String,
+          trim: true,
+        },
+        longitude: {
+          type: String,
+          trim: true,
+        },
+        isInitiated: {
+          type: Boolean,
+          default: false,
+          required: true,
+        },
+        recievedText: {
+          type: Boolean,
+          default: false,
+          required: true,
+        },
+      },
+      destinationLocation: {
+        longitude: {
+          type: String,
+          trim: true,
+        },
+        longitude: {
+          type: String,
+          trim: true,
+        },
+        isInitiated: {
+          type: Boolean,
+          default: false,
+          required: true,
+        },
+        recievedText: {
+          type: Boolean,
+          default: false,
+          required: true,
+        },
+      },
+      confirmLocation: {
+        isInitiated: {
+          type: Boolean,
+          default: false,
+          required: true,
+        },
+        recievedText: {
+          type: Boolean,
+          default: false,
+          required: true,
+        },
+        startLocationLink: {
+          type: String,
+          trim: true,
+        },
+        endLocationLink: {
+          type: String,
+          trim: true,
+        },
+      },
+      searchRides: {
+        isInitiated: {
+          type: Boolean,
+          default: false,
+          required: true,
+        },
+        driverDetails: {
+          type: String,
+          trim: true,
+        },
+        otp: {
+          type: String,
+          trim: true,
+        },
+      },
+      rideStatus: {
+        rideStarted: {
+          type: Boolean,
+          default: false,
+          required: true,
+        },
+        rideEnded: {
+          type: Boolean,
+          default: false,
+          required: true,
+        },
+      },
+    },
+  ],
 });
 
-const Rides = mongoose.model('Rides', userSchema);
+const Rides = mongoose.model("Rides", userSchema);
 
 module.exports = Rides;
